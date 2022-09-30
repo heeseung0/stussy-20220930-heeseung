2cc011a839cb585e2b3663b6790c4039be4621ca
heeseung0 <ys2645@naver.com>
2022-09-30 14:30:15 +0900

    no message

---
+++ src/test/java/com/stussy/stussyclone20220930heeseung/StussyClone20220930HeeseungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930heeseung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClone20220930HeeseungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
