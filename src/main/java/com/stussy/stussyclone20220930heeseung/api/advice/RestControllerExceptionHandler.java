c54abc1a51b778b4d8423acc6e4083b6990431bd
heeseung0 <ys2645@naver.com>
2022-10-06 17:47:14 +0900

    AOP,security start

---
+++ src/main/java/com/stussy/stussyclone20220930heeseung/api/advice/RestControllerExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.stussy.stussyclone20220930heeseung.api.advice;
+
+import com.stussy.stussyclone20220930heeseung.dto.CMRespDto;
+import com.stussy.stussyclone20220930heeseung.exception.CustomValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+@RestControllerAdvice
+public class RestControllerExceptionHandler {
+
+    @ExceptionHandler(CustomValidationException.class)
+    public ResponseEntity<?> validationErrorException(CustomValidationException e){
+        return ResponseEntity.badRequest().body(new CMRespDto<>(e.getMessage(),e.getErrorMap()));
+    }
+}
