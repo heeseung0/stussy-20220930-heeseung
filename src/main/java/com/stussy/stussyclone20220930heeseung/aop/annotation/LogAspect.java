6446fdc67fc6ec1a204a8ad84586b0c5d807ca51
heeseung <ys2645@naver.com>
2022-10-24 22:32:00 +0900

    필요없는 임포트 제거

--- src/main/java/com/stussy/stussyclone20220930heeseung/aop/annotation/LogAspect.java
+++ src/main/java/com/stussy/stussyclone20220930heeseung/aop/annotation/LogAspect.java
@@ -1,14 +1,5 @@
 package com.stussy.stussyclone20220930heeseung.aop.annotation;
 
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.CodeSignature;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.BeanPropertyBindingResult;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
